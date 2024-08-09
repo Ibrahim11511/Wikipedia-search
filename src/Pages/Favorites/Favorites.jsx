f634339cd4df458ed8591f75aaa2c52a33eadcfe
Ibrahim11511 <109541964+Ibrahim11511@users.noreply.github.com>
2024-08-09 19:40:02 +0300

    conditional render if the data from localStorage === 0

--- src/Pages/Favorites/Favorites.jsx
+++ src/Pages/Favorites/Favorites.jsx
@@ -2,6 +2,10 @@ import BoxsContainer from "../../Components/Boxs Container/BoxsContainer";
 import { getFavArticleFromLocalStorage } from "../../utils";
 import Styles from "./favorites.module.css";
 export default function Favorites() {
+  getFavArticleFromLocalStorage();
+  if (getFavArticleFromLocalStorage().length === 0) {
+    return <p className={Styles.noResult}>There is no favorites</p>;
+  }
   return (
     <section className={Styles.favorites}>
       <BoxsContainer apiResult={getFavArticleFromLocalStorage()} />
